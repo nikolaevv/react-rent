9a8c58704eb81012b66c0c5399f11b1d8da721c5
nikolaevv <starlei165@gmail.com>
2021-09-04 06:00:53 +0300

    feat: Add redux actions & reducer cases

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,26 @@
+const addBusinesses = (businesses) => {
+    return {
+        type: 'ADD_BUSINESSES',
+        payload: businesses
+    }
+};
 
+const setCurrentBusiness = (currentBusiness) => {
+    return {
+        type: 'SET_CURRENT_BUSINESS',
+        payload: currentBusiness
+    }
+};
+
+const addMessages = (messages) => {
+    return {
+        type: 'ADD_MESSAGES',
+        payload: messages
+    }
+};
 
 export {
-
+    addBusinesses,
+    setCurrentBusiness,
+    addMessages
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,29 @@
 const initialState = {
-
+    businesses: [],
+    currentBusiness: null,
+    messages: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'ADD_MESSAGES':
+            return {
+                ...state,
+                messages: action.payload
+            }
+
+        case 'ADD_BUSINESSES':
+            return {
+                ...state,
+                businesses: action.payload
+            }
+
+        case 'SET_CURRENT_BUSINESS':
+            return {
+                ...state,
+                currentBusiness: action.payload
+            }
+
         default:
             return state;
     }
